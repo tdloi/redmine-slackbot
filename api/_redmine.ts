ce7287015b9257bc3dbd9f172812c2bf79428400
Loi Tran <git@tdloi.net>
2019-11-26 13:10:39 +0700

    Add function to retrieve total logged hours

--- api/_redmine.ts
+++ api/_redmine.ts
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
-import { IRedmineIssue, IUserConfig } from './_interface';
+import { IRedmineIssue, IUserConfig, IRedmineTimeEntries } from './_interface';
 import { configs } from './_settings';
+import { Dayjs } from 'dayjs';
 
 export async function getIssues(
   userConfig: IUserConfig | null,
@@ -10,9 +11,8 @@ export async function getIssues(
   if (userConfig && userConfig.includeClosed) {
     status = 'open';
   }
-
   if (userConfig && userConfig.token) {
-    token = atob(userConfig.token);
+    token = Buffer.from(userConfig.token, 'base64').toString('ascii');
   }
 
   const issues: Array<IRedmineIssue> = await fetch(
@@ -27,4 +27,21 @@ export async function getIssues(
     .catch(_ => null);
 
   return issues;
+}
+
+export async function getLoggedHours(userConfig: IUserConfig, date: Dayjs): Promise<number> {
+  const loggedDate = date.format('YYYY-MM-DD');
+  const response: IRedmineTimeEntries = await fetch(
+    `${configs.REDMINE_URL}/time_entries.json?user_id=me&from=${loggedDate}&to=${loggedDate}`,
+    {
+      headers: {
+        'X-Redmine-API-Key': Buffer.from(userConfig.token, 'base64').toString('ascii'),
+      },
+    }
+  )
+    .then(res => res.json())
+    .catch(_ => {});
+  const timeEntries = response.time_entries || [];
+  const total = timeEntries.reduce((accumulator, curr) => accumulator + curr.hours, 0);
+  return total;
 }
