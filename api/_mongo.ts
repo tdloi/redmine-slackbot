0ba36eec82602c018867d15f6dbe07bf48fc32dc
Loi Tran <git@tdloi.net>
2019-11-28 08:59:10 +0700

    Add function to update user config

--- api/_mongo.ts
+++ api/_mongo.ts
@@ -1,6 +1,7 @@
 import url from 'url';
 import { MongoClient, Db } from 'mongodb';
 import { IGlobalConfig, IUserConfig } from './_interface';
+import { configs } from './_settings';
 // Create cached connection variable
 let cachedDb = null;
 
@@ -29,18 +30,30 @@ export async function getDb(): Promise<Db> {
 
 export async function getGlobalConfig(): Promise<IGlobalConfig> {
   const db = await getDb();
-  return db.collection('configs').findOne({ _type: 'globalConfig' });
+  return db.collection(configs.COLLECTION).findOne({ _type: 'globalConfig' });
 }
 
 export async function getUserConfig(userId: string): Promise<IUserConfig> {
   const db = await getDb();
-  return db.collection('configs').findOne({ _type: 'userConfig', userId: userId } as IUserConfig);
+  return db
+    .collection(configs.COLLECTION)
+    .findOne({ _type: 'userConfig', userId: userId } as IUserConfig);
+}
+
+export async function setUserConfig(userId: string, payload: IUserConfig): Promise<boolean> {
+  const db = await getDb();
+  const result = await db
+    .collection(configs.COLLECTION)
+    .updateOne({ _type: 'userConfig', userId: userId }, payload, {
+      upsert: true,
+    });
+  return result.matchedCount === 1 || result.upsertedCount === 1 || result.modifiedCount === 1;
 }
 
 export async function getUsersConfig(): Promise<Array<IUserConfig>> {
   const db = await getDb();
   return db
-    .collection('configs')
+    .collection(configs.COLLECTION)
     .find({ _type: 'userConfig' })
     .toArray();
 }
--- api/_settings.ts
+++ api/_settings.ts
@@ -9,11 +9,12 @@ export const configs = {
   SLACK_TOKEN: process.env.SLACK_BOT_TOKEN,
   SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET,
   AUTH_CRE: process.env.AUTH_CRE,
+  COLLECTION: 'configs',
 
   // DOES NOT include trailing slash
   // e.g: https://example.com
   REDMINE_URL: process.env.REDMINE_URL,
   WORK_HOURS: parseInt(process.env.WORK_HOURS),
-  TIMEZONE: parseInt(process.env.TIME_ZONE), // e.g: 7 or -7
+  TIMEZONE: parseInt(process.env.TIMEZONE), // e.g: 7 or -7
   SECRET: process.env.SECRET,
 };
