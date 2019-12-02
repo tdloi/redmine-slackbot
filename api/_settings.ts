fa41ea2d6c887a5c2af65d6773b883a472a8b2be
Loi Tran <git@tdloi.net>
2019-12-02 09:07:45 +0700

    Remove SLACK TEAM ID config

--- api/_settings.ts
+++ api/_settings.ts
@@ -7,7 +7,6 @@ export const actions = {
 };
 
 export const configs = {
-  SLACK_TEAM_ID: process.env.SLACK_TEAM_ID,
   SLACK_TOKEN: process.env.SLACK_BOT_TOKEN,
   SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET,
   AUTH_CRE: process.env.AUTH_CRE,
